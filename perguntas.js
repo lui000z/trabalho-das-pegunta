1d97be1bc0f109277ae583db14ec117a34173801
lui000z <luiz.alves.faria@escola.pr.gov.br>
2024-10-09 12:05:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'A capital da Argentina é Buenos Aires'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Matemática',
+    'O que é uma equação algébrica?',
+    'Uma equação algébrica é uma operação matemática que contém números e letras'
+)
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem inventou a lâmpada?',
+    'O inventor da lâmpada foi Thomas Edson'
+)
+criaCartao(
+    'Bíblico',
+    'Qual foi nome do homem que foi engolido por um peixe grande?',
+    'O homem que foi engolido por um peixe grande foi Jonas'
+)
+criaCartao(
+    'Matemática',
+    'Qual o nome da operação utilizada para resolver equações do 1º grau?',
+    'Para resolver equações de 1º grau utilizamos a operação inversa'
+)
